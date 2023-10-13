36e3bc398b455f59a5f221611badfd6a79f61574
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-13 22:06:30 +0800

    first app: change icon, build app and update app

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Saddam!</Text>
+      <Text style={{ marginTop: 30 }}>Version update</Text>
       <StatusBar style="auto" />
     </View>
   );
