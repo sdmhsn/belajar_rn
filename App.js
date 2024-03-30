505d763ea7776ffbc4bf3df1da04a23ff519187c
Saddam Husein <saddambinhusein2@gmail.com>
2024-03-30 13:54:41 +0800

    modify assets and App.js

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Saddam!</Text>
-      <Text style={{ marginTop: 30 }}>Version update</Text>
+      <Text style={styles.titleText}>Welcome to BelajarRN</Text>
+
+      <Text style={styles.bodyText}>Version update 1.1</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titleText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  bodyText: {
+    marginTop: 30,
+    color: '#545556',
   },
 });
